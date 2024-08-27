2d006178c2cd5832013f604650821e6ad4b28889
wdj <dingjie.wang@supwisdom.com>
2024-08-27 18:02:52 +0800

    router started

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
-import Login from './container/login'
+import { ROUTE_CONFIG } from './routes'
+import NotFound from './containers/NotFound'
 
 function App() {
-
   return (
-    <>
-      <Login/>
-    </>
+    <BrowserRouter>
+      {/* 路由集合 */}
+      <Routes>
+        {/* 可以优化写成一个路由列表 */}
+        {ROUTE_CONFIG.map((item) => {
+          return (<Route
+            key={item.key}
+            path={item.path}
+            element={<item.element />}
+          />)
+        })}
+        <Route path='*' element={ <NotFound/> } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import Home from '../containers/Home'
+import Login from '../containers/Login'
+import NotFound from '../containers/NotFound'
+export const ROUTE_CONFIG = [
+    {
+        key: 'home',
+        path: '/',
+        element: Home,
+        title: '首页'
+    },
+    {
+        key: 'login',
+        path: '/login',
+        element: Login,
+        title: '登录'
+    },
+    {
+        key: 'NotFound',
+        path: '*',
+        element: NotFound,
+        title: '未找到页面'
+    }
+]
