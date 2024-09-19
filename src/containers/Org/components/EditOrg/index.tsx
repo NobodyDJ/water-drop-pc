7acf9df2e63459d01ec94dae15996138b7f926d9
NobodyDJ <108987166+NobodyDJ@users.noreply.github.com>
2024-09-19 18:15:25 +0800

    feat: org unfinished

--- src/containers/Org/components/EditOrg/index.tsx
+++ src/containers/Org/components/EditOrg/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
 
 import style from './index.module.less';
-import { Drawer } from 'antd';
+import { Col, Divider, Drawer, Form, Input, Row, Select } from 'antd';
+import OSSImageUpload from '@/components/OSSImageUpload';
 
 interface IProp{
     id: string;
@@ -14,7 +15,8 @@ interface IProp{
 const EditOrg = ({
     id,
     onClose
-}:IProp) => {
+}: IProp) => {
+    const [form] = Form.useForm();
     useEffect(() => {
         console.log(id, onClose);
     }, []);
@@ -24,9 +26,152 @@ const EditOrg = ({
         onClose={onClose}
         open
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <Form form={form} initialValues={initValue} layout="vertical">
+            <Row className={style.row} gutter={20}>
+            <Col span={10}>
+                <Form.Item
+                style={{ width: '100%' }}
+                label="Logo"
+                name="logo"
+                rules={[{ required: true }]}
+                >
+                <OSSImageUpload
+                    maxCount={1}
+                    label="替换 Logo"
+                />
+                </Form.Item>
+            </Col>
+            <Col span={14}>
+                <Form.Item
+                style={{ width: '100%' }}
+                label="名称"
+                name="name"
+                rules={[{ required: true }]}
+                >
+                <Input placeholder="请输入门店名称" />
+                </Form.Item>
+            </Col>
+            </Row>
+            <Row className={style.row} gutter={20}>
+            <Col span={11}>
+                <Form.Item
+                label="标签"
+                name="tags"
+                rules={[{ required: true }]}
+                >
+                <Select
+                    mode="tags"
+                    style={{ width: '100%' }}
+                    placeholder="请输入标签"
+                />
+                </Form.Item>
+            </Col>
+            <Col span={5}>
+                <Form.Item
+                label="手机号"
+                name="tel"
+                rules={[{ required: true }]}
+                >
+                <Input placeholder="请输入手机号" />
+                </Form.Item>
+            </Col>
+            <Col span={4}>
+                <Form.Item
+                label="经度"
+                name="longitude"
+                rules={[{ required: true }]}
+                >
+                <Input placeholder="请输入经度" />
+                </Form.Item>
+            </Col>
+            <Col span={4}>
+                <Form.Item
+                label="纬度"
+                name="latitude"
+                rules={[{ required: true }]}
+                >
+                <Input placeholder="请输入纬度" />
+                </Form.Item>
+            </Col>
+            </Row>
+            <Form.Item
+            label="地址"
+            name="address"
+            rules={[{ required: true }]}
+            >
+            <Input placeholder="请输入地址" />
+            </Form.Item>
+            <Form.Item
+            label="门店简介"
+            name="description"
+            rules={[{ required: true }]}
+            >
+            <Input.TextArea
+                maxLength={500}
+                rows={5}
+                className={style.textArea}
+                allowClear
+                showCount
+            />
+            </Form.Item>
+            <Row className={style.row} gutter={20}>
+            <Col span={8}>
+                <Form.Item
+                style={{ width: '100%' }}
+                label="营业执照"
+                name="businessLicense"
+                rules={[{ required: true }]}
+                >
+                <OSSImageUpload
+                    label="替换营业执照"
+                    maxCount={1}
+                    imgCropAspect={3 / 2}
+                />
+                </Form.Item>
+            </Col>
+            <Col span={8}>
+                <Form.Item
+                style={{ width: '100%' }}
+                label="身份证正面"
+                name="identityCardFrontImg"
+                rules={[{ required: true }]}
+                >
+                <OSSImageUpload
+                    label="替换身份证"
+                    maxCount={1}
+                    imgCropAspect={3 / 2}
+                />
+                </Form.Item>
+            </Col>
+            <Col span={8}>
+                <Form.Item
+                style={{ width: '100%' }}
+                label="身份证背面"
+                name="identityCardBackImg"
+                rules={[{ required: true }]}
+                >
+                <OSSImageUpload
+                    label="替换身份证"
+                    maxCount={1}
+                    imgCropAspect={3 / 2}
+                />
+                </Form.Item>
+            </Col>
+            </Row>
+
+            <Divider>门店顶部图：图片长宽要求比例为 2:1，最多上传 5 张 </Divider>
+            <Form.Item name="orgFrontImg">
+            <UploadImage maxCount={5} imgCropAspect={2 / 1} />
+            </Form.Item>
+            <Divider>门店室内图：图片长宽要求比例为 2:1，最多上传 5 张 </Divider>
+            <Form.Item name="orgRoomImg">
+            <UploadImage maxCount={5} imgCropAspect={2 / 1} />
+            </Form.Item>
+            <Divider>门店其他图：图片长宽要求比例为 2:1，最多上传 5 张 </Divider>
+            <Form.Item name="orgOtherImg">
+            <UploadImage maxCount={5} imgCropAspect={2 / 1} />
+            </Form.Item>
+        </Form>
       </Drawer>);
 };
 
